4ebdab7bccd40aa6c954923c9640bfacf758b796
Alexandro Abade <alexandroabade@gmail.com>
2023-10-09 18:41:41 -0300

    Configuração e ambientação do ecosistema de packages dependências do Java

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# curso-de-java
+# Curso de JAVA - Rocketseat
 Nesse curso de JAVA pela Rocketseat 💜 iremos trabalhar a construção back-end de uma aplicação de To-Do List
---
+++ todolist/src/main/java/br/com/alexandroabade/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.alexandroabade.todolist.user;
+
+public class UserModel {
+    
+    public String username;
+    public String name;
+    public String password;
+}
